33821645d1e24495f23fc36467f5dd06e656a4c1
Madhura <madhura.bhujbal@gmail.com>
2020-04-21 17:29:36 +0100

    Retrieve weather data either from database or from web

--- weather.js
+++ weather.js
@@ -2,13 +2,17 @@ const axios = require('axios');
 const {MongoClient} = require('mongodb');
 
 let currentDate = new Date();
-console.log(currentDate);
 let day = currentDate.getDate();
 let month = currentDate.getMonth();
 let year = currentDate.getUTCFullYear();
-let date = day + '' + ('0' + (month + 1)) + '' + year;
-console.log(date);
+let timestamp = day + '' + ('0' + (month + 1)) + '' + year;
 let city = 'London';
+let weatherInfo;
+
+function getOnlineInfo() {
+    //1. Fetch data from weather site
+    return axios.get('https://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=439d4b804bc8187953eb36d2a8c26a02');
+}
 
 //Establish connection to database
 const url = 'mongodb://localhost:27017';
@@ -18,30 +22,20 @@ const url = 'mongodb://localhost:27017';
         conn = await MongoClient.connect(url, {useUnifiedTopology: true});
         const db = conn.db('WeatherApp');
         const collection = db.collection('weatherData');
-        const result = await collection.findOne();
-        console.log(result);
-    } catch (error) {
-       console.log('Error: ' + error);
-    }
+        const result = await collection.findOne({"city" : city, "date" : timestamp});
 
+        if(result){
+            console.log('Retrieving from database');
+            weatherInfo = result.data;
+        } else{
+            console.log('Retrieving from web');
+            let response = await getOnlineInfo();
+            weatherInfo = response.data;
+        }
+        console.log(weatherInfo);
+    } catch (error) {
+        console.log('Error: ' + error);
+    }
     //Close connection
     conn.close();
-    }());
-
-function getOnlineInfo() {
-    //1. Fetch data from weather site
-    axios.get('https://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=439d4b804bc8187953eb36d2a8c26a02')
-    .then((response) => {
-
-        //2. Display fetched data on console
-        return response.data;
-    })
-    .catch((error) => {
-        console.log('error : ' + error);
-    });
-
-}
-
-    //Retrieve data from database
-
-
+}());
