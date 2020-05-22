7717bd9210992c7e706dd646bcde818abcbf733e
Madhura <madhura.bhujbal@gmail.com>
2020-05-22 19:26:20 +0100

    Convert synchronous function to async function and make code changes accordingly

--- synchronousWeatherAPI.js
+++ synchronousWeatherAPI.js
@@ -4,24 +4,20 @@ const {MongoClient} = require('mongodb');
 let cityName = 'Manchester';
 
 //establish connection
-MongoClient.connect('mongodb://localhost:27017', function(err, conn) {
-    if (err) throw err;
+(async function () {
+    let conn = await MongoClient.connect('mongodb://localhost:27017');
     //connect to database
     dbRef = conn.db('WeatherApp');
     collRef = dbRef.collection('weatherData');
-    collRef.findOne({"city" : cityName}, function(err, result) {
-        if(err) throw err;
-        if(result != null) {
+    let weatherInfo = await collRef.findOne({"city" : cityName});
+        if(weatherInfo != null) {
             console.log("Retrieving data from database : ");
-            console.log(result);
+            console.log(weatherInfo);
         } else {
-            (async function () {
                 console.log("Retrieving data from website : ");
-                result = await axios.get('http://api.openweathermap.org/data/2.5/weather?q=manchester&appid=446cc30bc37be1228e1d55b09335aeb6');
-                console.log(result);
-            }());
-        }
-    });
-    //close connection
-    conn.close();
-});
+                weatherInfo = await axios.get('http://api.openweathermap.org/data/2.5/weather?q=manchester&appid=446cc30bc37be1228e1d55b09335aeb6');
+                console.log(weatherInfo);
+            }
+        //close connection
+        conn.close();
+}());
