58c5045d98c1fddc3a50b88894241397b1987940
Madhura <madhura.bhujbal@gmail.com>
2020-05-23 18:52:57 +0100

    Write async function to retrieve city weather data dynamically

--- synchronousWeatherAPI.js
+++ synchronousWeatherAPI.js
@@ -1,27 +1,32 @@
 const axios = require('axios');
 const {MongoClient} = require('mongodb');
 
-let cityName = 'Manchester';
 
-//establish connection
-(async function () {
-    let conn = await MongoClient.connect('mongodb://localhost:27017');
-    //connect to database
-    dbRef = conn.db('WeatherApp');
-    collRef = dbRef.collection('weatherData');
-    let weatherInfo = await collRef.findOne({"city" : cityName});
-    if(weatherInfo != null) {
-        console.log("Retrieving data from database : ");
-        console.log(weatherInfo);
-    } else {
-            console.log("Retrieving data from website : ");
-            weatherInfo = await axios.get('http://api.openweathermap.org/data/2.5/weather?q=manchester&appid=446cc30bc37be1228e1d55b09335aeb6');
-            console.log(weatherInfo.data);
-            collRef.insertOne({"city" : cityName}, function(err, res) {
-                if (err) throw err;
-                console.log('Weather info inserted');
-            });
-        }
-    //close connection
-    conn.close();
-}());
+function getTimeStamp() {
+    return "20200523";
+}
+
+function getWeatherInfo(cityName) {
+    let weatherInfo = null;
+    //establish connection
+    (async function () {
+        let conn = await MongoClient.connect('mongodb://localhost:27017');
+        //connect to database
+        dbRef = conn.db('WeatherApp');
+        collRef = dbRef.collection('weatherData');
+        weatherInfo = await collRef.findOne({"city" : cityName});
+        if(weatherInfo != null) {
+            console.log("Retrieving data from database : ");
+        } else {
+                console.log("Retrieving data from website : ");
+                weatherInfo = await axios.get("http://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=446cc30bc37be1228e1d55b09335aeb6");
+                let result = await collRef.insertOne({"city" : cityName, date: getTimeStamp(), "data" : weatherInfo.data});
+            }
+        //close connection
+        conn.close();
+        console.log(weatherInfo.data);
+        return weatherInfo.data;
+    }());
+}
+
+module.exports = getWeatherInfo;
