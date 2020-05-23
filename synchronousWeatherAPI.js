3c3b33339f9e1a3d7b710a829b9fb79008936a09
Madhura <madhura.bhujbal@gmail.com>
2020-05-23 17:59:49 +0100

    Get data from web and store in database

--- synchronousWeatherAPI.js
+++ synchronousWeatherAPI.js
@@ -10,14 +10,18 @@ let cityName = 'Manchester';
     dbRef = conn.db('WeatherApp');
     collRef = dbRef.collection('weatherData');
     let weatherInfo = await collRef.findOne({"city" : cityName});
-        if(weatherInfo != null) {
-            console.log("Retrieving data from database : ");
-            console.log(weatherInfo);
-        } else {
-                console.log("Retrieving data from website : ");
-                weatherInfo = await axios.get('http://api.openweathermap.org/data/2.5/weather?q=manchester&appid=446cc30bc37be1228e1d55b09335aeb6');
-                console.log(weatherInfo);
-            }
-        //close connection
-        conn.close();
+    if(weatherInfo != null) {
+        console.log("Retrieving data from database : ");
+        console.log(weatherInfo);
+    } else {
+            console.log("Retrieving data from website : ");
+            weatherInfo = await axios.get('http://api.openweathermap.org/data/2.5/weather?q=manchester&appid=446cc30bc37be1228e1d55b09335aeb6');
+            console.log(weatherInfo.data);
+            collRef.insertOne({"city" : cityName}, function(err, res) {
+                if (err) throw err;
+                console.log('Weather info inserted');
+            });
+        }
+    //close connection
+    conn.close();
 }());
