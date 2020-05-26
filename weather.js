e62217822383ea0a6e917aa594eee99df0fde958
Madhura <madhura.bhujbal@gmail.com>
2020-05-26 12:09:44 +0100

    Install express and ejs packages

--- weather.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const axios = require('axios');
-const {MongoClient} = require('mongodb');
-
-const app = express();
-app.set('views', __dirname + '/views/');
-app.set('view engine', 'ejs');
