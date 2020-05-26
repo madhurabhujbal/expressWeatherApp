90df516c69b6b22b9acbfa1c25dc14485ba2cd36
Madhura <madhura.bhujbal@gmail.com>
2020-05-26 12:11:01 +0100

    Render index file contents on port

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const axios = require('axios');
 const {MongoClient} = require('mongodb');
+const {getWeatherInfo, getTimeStamp} = require('./synchronousWeatherAPI');
 
 const app = express();
 app.set('views', __dirname + '/views/');
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-    //res.render('index');
-    res.send("Hello World");
+    res.render('index');
 });
 
 app.listen(3000, () => {
