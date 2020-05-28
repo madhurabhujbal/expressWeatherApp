19dc1f4cc994fb36596151cfd72776cb0f2172a2
Madhura <madhura.bhujbal@gmail.com>
2020-05-28 09:10:13 +0100

    Modify code to display temperature of cityon port

--- app.js
+++ app.js
@@ -12,9 +12,26 @@ app.get('/', (req, res) => {
 });
 
 app.get('/weather', (req, res) => {
-    let city = req.query.City;
+    let cityName = req.query.City;
     let currentDate = getTimeStamp();
-    res.render('weatherPage', {city : city, date : currentDate} );
+    (async function weather() {
+        let conn = await MongoClient.connect('mongodb://localhost:27017');
+        //connect to database
+        dbRef = conn.db('WeatherApp');
+        collRef = dbRef.collection('weatherData');
+        weatherInfo = await collRef.findOne({"city" : cityName});
+        if(weatherInfo != null) {
+            console.log("Retrieving data from database : ");
+        } else {
+                console.log("Retrieving data from website : ");
+                weatherInfo = await axios.get("http://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=446cc30bc37be1228e1d55b09335aeb6");
+                let result = await collRef.insertOne({"city" : cityName, date: getTimeStamp(), "data" : weatherInfo.data});
+            }
+        //close connection
+        conn.close();
+        console.log(weatherInfo);
+        res.render('weatherPage', {city : cityName, date : currentDate, weatherData : weatherInfo} );
+    }());
 });
 
 app.listen(3000, () => {
