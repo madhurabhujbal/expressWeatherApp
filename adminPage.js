a188665195b0164e522297999429d6f6dd6ca19b
Madhura <madhura.bhujbal@gmail.com>
2020-06-04 13:25:20 +0100

    Add code to connect to DB. Send hard coded value for display

--- adminPage.js
+++ adminPage.js
@@ -1,9 +1,22 @@
+const {MongoClient} = require('mongodb');
+const {getTimeStamp} = require('./synchronousWeatherAPI');
 
 
-function deleteOldRecords() {
-    if(weatherInfo.date != currentDate) {
-        await collRef.deleteMany({"city" : cityName});
-    }
+function deleteOldRecords(res) {
+    let currentDate = getTimeStamp();
+//establish connection to database
+    (async function() {
+        let url = 'mongodb://localhost:27017';
+        let conn = await MongoClient.connect(url);
+        let dbRef = conn.db('WeatherApp');
+        let coll = dbRef.collection('weatherData');
+        //delete old records
+        //let queryResult = await coll.deleteMany({"date": {$lt: currentDate}});
+        //close connection
+        conn.close();
+        //console.log(queryResult.deletedCount);
+        res.render('adminPage', {recordNumber: 10});
+    }());
 }
 
 module.exports = deleteOldRecords;
