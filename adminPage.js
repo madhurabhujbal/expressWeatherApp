e28108482cb6b1449e654df4a9fa802683537921
Madhura <madhura.bhujbal@gmail.com>
2020-06-05 10:33:53 +0100

    Wrap the code in try-catch block

--- adminPage.js
+++ adminPage.js
@@ -8,10 +8,14 @@ function deleteOldRecords(res) {
     (async function() {
         let url = 'mongodb://localhost:27017';
         let conn = await MongoClient.connect(url);
-        let dbRef = conn.db('WeatherApp');
-        let coll = dbRef.collection('weatherData');
-        //delete old records
-        let queryResult = await coll.deleteMany({"date": {$lt: currentDate}});
+        try {
+            let dbRef = conn.db('WeatherApp');
+            let coll = dbRef.collection('weatherData');
+            //delete old records
+            let queryResult = await coll.deleteMany({"date": {$lt: currentDate}});
+        } catch (err) {
+            console.log(err);
+        }
         //close connection
         conn.close();
         console.log(queryResult.deletedCount);
