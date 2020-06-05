90bf1958e27c8bf994ce80713c3cf23aa69a200b
Madhura <madhura.bhujbal@gmail.com>
2020-06-05 10:21:35 +0100

    Wrap code in try catch block

--- synchronousWeatherAPI.js
+++ synchronousWeatherAPI.js
@@ -21,17 +21,22 @@ function getWeatherInfo(cityName, res, currentDate) {
     //establish connection
     (async function () {
         let conn = await MongoClient.connect('mongodb://localhost:27017');
-        //connect to database
-        dbRef = conn.db('WeatherApp');
-        collRef = dbRef.collection('weatherData');
-        weatherInfo = await collRef.findOne({"city" : cityName, "date": currentDate});
-        if(weatherInfo != null) {
-            console.log("Retrieving data from database : ");
-        } else {
+        try {
+            //connect to database
+            dbRef = conn.db('WeatherApp');
+            collRef = dbRef.collection('weatherData');
+            weatherInfo = await collRef.findOne({"city" : cityName, "date": currentDate});
+            if(weatherInfo != null) {
+                console.log("Retrieving data from database : ");
+            } else {
                 console.log("Retrieving data from website : ");
                 weatherInfo = await axios.get("http://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=446cc30bc37be1228e1d55b09335aeb6");
                 let result = await collRef.insertOne({"city" : cityName, date: getTimeStamp(), "data" : weatherInfo.data});
             }
+        }
+        catch(err) {
+            console.log(err);
+        }
         //close connection
         conn.close();
         console.log(weatherInfo.data);
